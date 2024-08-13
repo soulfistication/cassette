27a93c56e92f2bf91cadfd42abdfdd91537ad214
Ivan Almada <ivan.almada@me.com>
2024-08-12 21:12:03 -0600

    Initial commit

---
+++ cassette/cassetteApp.swift
@@ -0,0 +1,17 @@
+//
+//  cassetteApp.swift
+//  cassette
+//
+//  Created by Ivan Almada on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct cassetteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
